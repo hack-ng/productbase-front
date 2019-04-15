103cdb096de92e9bdc4658fb1ddbf3361d8ddd05
teenoh <ugbalaachinike@gmail.com>
2019-04-15 15:11:29 +0100

    setup redux for state management

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
+import { Provider } from 'react-redux'
+
+import { configureStore } from "./store"
+
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
@@ -9,13 +13,21 @@ import "assets/scss/argon-dashboard-react.scss";
 import AdminLayout from "layouts/Admin.jsx";
 import AuthLayout from "layouts/Auth.jsx";
 
-ReactDOM.render(
+const store = configureStore()
+
+const Index = () => (
+  <Provider store={store}>
   <BrowserRouter>
     <Switch>
       <Route path="/admin" render={props => <AdminLayout {...props} />} />
       <Route path="/auth" render={props => <AuthLayout {...props} />} />
       <Redirect from="/" to="/admin/index" />
     </Switch>
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>
+)
+
+ReactDOM.render(
+  <Index />,
   document.getElementById("root")
 );
