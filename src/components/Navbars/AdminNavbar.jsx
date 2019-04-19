5146691c131c2c3a250902c13d774369345c03b5
teenoh <ugbalaachinike@gmail.com>
2019-04-19 04:11:37 +0100

    refactor admin navbar

--- src/components/Navbars/AdminNavbar.jsx
+++ src/components/Navbars/AdminNavbar.jsx
@@ -22,7 +22,11 @@ class AdminNavbar extends React.Component {
   render() {
     return (
       <>
-        <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
+        <Navbar
+          className="navbar-top navbar-dark"
+          expand="md"
+          id="navbar-main"
+        >
           <Container fluid>
             <Link
               className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block"
@@ -30,7 +34,7 @@ class AdminNavbar extends React.Component {
             >
               {this.props.brandText}
             </Link>
-            <Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
+            {/* <Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
               <FormGroup className="mb-0">
                 <InputGroup className="input-group-alternative">
                   <InputGroupAddon addonType="prepend">
@@ -41,9 +45,14 @@ class AdminNavbar extends React.Component {
                   <Input placeholder="Search" type="text" />
                 </InputGroup>
               </FormGroup>
-            </Form>
+            </Form> */}
             <Nav className="align-items-center d-none d-md-flex" navbar>
-              <UncontrolledDropdown nav>
+              <Media className="ml-2 d-none d-lg-block">
+                <span className="mb-0 text-sm font-weight-bold text-white">
+                  Jessica Jones
+                </span>
+              </Media>
+              {/* <UncontrolledDropdown nav>
                 <DropdownToggle className="pr-0" nav>
                   <Media className="align-items-center">
                     <span className="avatar avatar-sm rounded-circle">
@@ -80,12 +89,15 @@ class AdminNavbar extends React.Component {
                     <span>Support</span>
                   </DropdownItem>
                   <DropdownItem divider />
-                  <DropdownItem href="#pablo" onClick={e => e.preventDefault()}>
+                  <DropdownItem
+                    href="#pablo"
+                    onClick={e => e.preventDefault()}
+                  >
                     <i className="ni ni-user-run" />
                     <span>Logout</span>
                   </DropdownItem>
                 </DropdownMenu>
-              </UncontrolledDropdown>
+              </UncontrolledDropdown> */}
             </Nav>
           </Container>
         </Navbar>
