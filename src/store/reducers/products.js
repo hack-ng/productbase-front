05e9054bd8ad9b83aa736289f980f7b8178a52cf
teenoh <ugbalaachinike@gmail.com>
2019-04-24 02:17:22 +0100

    setup product reducers and actions

---
+++ src/store/reducers/products.js
@@ -0,0 +1,41 @@
+import {
+  FETCH_PRODUCTS_PENDING,
+  FETCH_PRODUCTS_REJECTED,
+  FETCH_PRODUCTS_FULFILLED
+} from "../actions/constants";
+
+const initialState = {
+  products: [],
+  loading: false,
+  error: null
+};
+
+const productsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_PRODUCTS_PENDING:
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+
+    case FETCH_PRODUCTS_REJECTED:
+      return {
+          ...state,
+          loading: false,
+          error: true
+      }
+
+    case FETCH_PRODUCTS_FULFILLED:
+      return {
+          ...state,
+          loading: false,
+          products: action.payload
+      }
+
+    default:
+      return state;
+  }
+};
+
+export default productsReducer;
