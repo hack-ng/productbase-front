2fc0af3ec8597a6634e56982a371a737057b420d
teenoh <ugbalaachinike@gmail.com>
2019-04-24 03:20:36 +0100

    update root reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux'
+import productsReducer from "./products"
+import authReducer from "./auth"
 
 const rootReducer = combineReducers({
-
+    products: productsReducer,
+    auth: authReducer,
 })
 
 export default rootReducer
