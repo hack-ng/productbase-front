62bd3e3531e72f104b998f7590a6f1121be0a761
teenoh <ugbalaachinike@gmail.com>
2019-04-24 05:44:54 +0100

    setup redux persist

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,9 +2,23 @@ import { combineReducers } from 'redux'
 import productsReducer from "./products"
 import authReducer from "./auth"
 
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    stateReconciler: autoMergeLevel2
+}
+
 const rootReducer = combineReducers({
     products: productsReducer,
     auth: authReducer,
 })
 
-export default rootReducer
+const pReducer = persistReducer(persistConfig, rootReducer);
+
+
+
+export default pReducer
