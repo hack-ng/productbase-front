5c354a48ccdc7504b55d46f34ac9525f3985fefc
teenoh <ugbalaachinike@gmail.com>
2019-04-25 01:21:30 +0100

    populate summary cards with redux data

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -3,112 +3,115 @@ import React from "react";
 // reactstrap components
 import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
 
+import { connect } from "react-redux";
+
 class Header extends React.Component {
 
   renderSummary = () => {
+    const { entries_stats } = this.props.user;
     return (
       <>
-      {/* Card stats */}
-            <Row>
-              <Col lg="6" xl="3">
-                <Card className="card-stats mb-4 mb-xl-0">
-                  <CardBody>
-                    <Row>
-                      <div className="col">
-                        <CardTitle
-                          tag="h5"
-                          className="text-uppercase text-muted mb-0"
-                        >
-                          Entries
-                        </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">
-                          350
-                        </span>
-                      </div>
-                      <Col className="col-auto">
-                        <div className="icon icon-shape bg-info text-white rounded-circle shadow">
-                          <i className="fas fa-chart-bar" />
-                        </div>
-                      </Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Col>
-              <Col lg="6" xl="3">
-                <Card className="card-stats mb-4 mb-xl-0">
-                  <CardBody>
-                    <Row>
-                      <div className="col">
-                        <CardTitle
-                          tag="h5"
-                          className="text-uppercase text-muted mb-0"
-                        >
-                          Approved
-                        </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">
-                          2,356
-                        </span>
-                      </div>
-                      <Col className="col-auto">
-                        <div className="icon icon-shape bg-success text-white rounded-circle shadow">
-                          <i className=" ni ni-check-bold" />
-                        </div>
-                      </Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Col>
-              <Col lg="6" xl="3">
-                <Card className="card-stats mb-4 mb-xl-0">
-                  <CardBody>
-                    <Row>
-                      <div className="col">
-                        <CardTitle
-                          tag="h5"
-                          className="text-uppercase text-muted mb-0"
-                        >
-                          Rejected
-                        </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">
-                          924
-                        </span>
-                      </div>
-                      <Col className="col-auto">
-                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                          <i className=" ni ni-fat-remove" />
-                        </div>
-                      </Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Col>
-              <Col lg="6" xl="3">
-                <Card className="card-stats mb-4 mb-xl-0">
-                  <CardBody>
-                    <Row>
-                      <div className="col">
-                        <CardTitle
-                          tag="h5"
-                          className="text-uppercase text-muted mb-0"
-                        >
-                          Pending
-                        </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">
-                          49
-                        </span>
-                      </div>
-                      <Col className="col-auto">
-                        <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
-                          <i className="ni ni-button-pause" />
-                        </div>
-                      </Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-            </>
-    )
+        {/* Card stats */}
+        <Row>
+          <Col lg="6" xl="3">
+            <Card className="card-stats mb-4 mb-xl-0">
+              <CardBody>
+                <Row>
+                  <div className="col">
+                    <CardTitle
+                      tag="h5"
+                      className="text-uppercase text-muted mb-0"
+                    >
+                      Entries
+                    </CardTitle>
+                    <span className="h2 font-weight-bold mb-0">
+                      {entries_stats.total}
+                    </span>
+                  </div>
+                  <Col className="col-auto">
+                    <div className="icon icon-shape bg-info text-white rounded-circle shadow">
+                      <i className="fas fa-chart-bar" />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="6" xl="3">
+            <Card className="card-stats mb-4 mb-xl-0">
+              <CardBody>
+                <Row>
+                  <div className="col">
+                    <CardTitle
+                      tag="h5"
+                      className="text-uppercase text-muted mb-0"
+                    >
+                      Approved
+                    </CardTitle>
+                    <span className="h2 font-weight-bold mb-0">
+                      {entries_stats.approved}
+                    </span>
+                  </div>
+                  <Col className="col-auto">
+                    <div className="icon icon-shape bg-success text-white rounded-circle shadow">
+                      <i className=" ni ni-check-bold" />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="6" xl="3">
+            <Card className="card-stats mb-4 mb-xl-0">
+              <CardBody>
+                <Row>
+                  <div className="col">
+                    <CardTitle
+                      tag="h5"
+                      className="text-uppercase text-muted mb-0"
+                    >
+                      Rejected
+                    </CardTitle>
+                    <span className="h2 font-weight-bold mb-0">
+                      {entries_stats.rejected}
+                    </span>
+                  </div>
+                  <Col className="col-auto">
+                    <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                      <i className=" ni ni-fat-remove" />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="6" xl="3">
+            <Card className="card-stats mb-4 mb-xl-0">
+              <CardBody>
+                <Row>
+                  <div className="col">
+                    <CardTitle
+                      tag="h5"
+                      className="text-uppercase text-muted mb-0"
+                    >
+                      Pending
+                    </CardTitle>
+                    <span className="h2 font-weight-bold mb-0">
+                      {entries_stats.pending}
+                    </span>
+                  </div>
+                  <Col className="col-auto">
+                    <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
+                      <i className="ni ni-button-pause" />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </>
+    );
   }
 
 
@@ -125,4 +128,17 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    user: state.auth.user
+  };
+};
+
+const mapDispatchToProps = {};
+
+const HeaderWithRedux = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
+
+export default HeaderWithRedux;
