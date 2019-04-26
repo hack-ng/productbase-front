3cb9eb5aa722115efdbc42a882f3ebd4f1031713
teenoh <ugbalaachinike@gmail.com>
2019-04-26 10:26:37 +0100

    black list products and entries in redux-persist

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -9,10 +9,11 @@ import storage from 'redux-persist/lib/storage'
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
 const persistConfig = {
-    key: 'root',
-    storage: storage,
-    stateReconciler: autoMergeLevel2
-}
+  key: "root",
+  storage: storage,
+  stateReconciler: autoMergeLevel2,
+  blacklist: ["products", "entries"]
+};
 
 const rootReducer = combineReducers({
     products: productsReducer,
